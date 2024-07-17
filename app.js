f338822e819df31c249ae2753abbe81c0874bcde
jeeteshkumar99 <jeeteshkumasuthar2003@gmail.com>
2024-07-17 21:09:17 +0500

    js assignment

---
+++ app.js
@@ -0,0 +1,171 @@
+// if else statements
+
+// // 1- Write a program to check whether the given input number is divisible or not
+
+// var num= +prompt("Enter a number ")
+// if (num % 3 == 0){
+//     document.write("Number is dvisible by 3")
+// }else{
+//     document.write("Number is not dvisible by 3")
+// }
+
+
+
+// // write a program number is even or not
+
+// var a = +prompt("Enter a number")
+// if (a % 2 == 0){
+//     document.write("even")
+// }else{
+//     document.write("odd")
+// }
+
+
+
+// // 3- if age is >= 18 old enough else young enough
+
+// var b =+ prompt("enter anumber")
+// if(b >=18){
+//     document.write("Old enough")
+// }else{
+//     document.write("Young enough")
+// }
+
+
+
+// // 4-
+
+// var name = prompt("Enter your name")
+// var myName ="Jeetesh"
+// if(name == myName){
+//     alert("Greetings !")
+// }else{
+//     alert("error")
+// }
+
+
+// // 6 - upper case and lowercase
+
+// var c = prompt("Enter a character")
+// if( c >= 'A'  && c <= 'Z'){
+//     document.write("Upercase")
+// }else if ( c >= 'a'  && c <= 'z'){
+//     document.write("lower case")
+// }else{
+//     document.write("Not a character")
+// }
+
+
+// // 7- time clock
+
+
+// var time = +prompt("Enter a time")
+// if(time ==1900){
+//     document.write("7 PM")
+// }else if(time >= 0 && time < 1200){
+//     document.write("Good morning")
+// }else if (time >= 1200 && time < 1700){
+//     document.write("Good After noon")
+// }else if (time >= 1700 && time < 2100){
+//     document.write("Good evening")
+// }else if (time >= 2100 && time < 2359){
+//     document.write("Good night")
+// }else{
+//     document.write("error")
+// }
+
+
+// // 9- leap year or not
+
+// var year = +prompt("Enter year ")
+// if(year % 4 == 0){
+//     document.write("leap year ")
+// }else{
+//     document.write("not a leap year")
+// }
+
+// // password is correct or not
+
+// var js = 5607
+// var password = +prompt("Enter you password")
+// if(password == js){
+//     document.write("correct")
+// }else{
+//     document.write("incorrect")
+// }
+
+
+// // 11-
+// var firstName =  prompt("Enter name")
+// if(firstName === "Fahad"){
+//     document.write("Hello fahad")
+// }else{
+//     document.write("You are not fahad")
+// }
+
+// // 12- 
+
+// var hour = +prompt("Enter anumber")
+
+// if(hour < 18){
+    
+//     greeting ="Good day"
+// }else{
+//     greeting ="Good evening"
+// }
+// document.write(greeting)
+
+
+// // 14- Display large number
+
+// var d = +prompt("Enter a mumber 1")
+// var e = +prompt("Enter a mumber 2")
+
+// if(d > e){
+//     document.write("larger is "+d)
+// }else if(e > d){
+//     document.write("larger is "+e)
+// }else if(d == e){
+//     document.write("Number is equal")
+// }else{
+//     document.write("Not a number")
+// }
+
+// // 15- Display number is positive or negative
+
+// var f = +prompt("Enter number 1")
+
+// if(f >= 0){
+//     document.write("Number is Positive")
+// }else if(f < 0){
+//     document.write("number is negative")
+// }else{
+//     document.write("Not a number")
+// }
+
+// // 17- check vowel
+
+// var g = prompt("Enter a character")
+// if(g === "a" || g === "e" || g === "i" || g === "o" || g === "u"){
+//     document.write("Vowel")
+// }else{
+//     document.write("False")
+// }
+
+// 18- 
+    
+    // not equal to 10 !- 8
+
+
+// // 20- use ternary opr  age is greater than 18 or not
+
+// var age = +prompt("Enter number")
+// if(age >= 18){
+//     alert("old enough")
+// }else{
+//     alert("young enough")
+// }
+
+
+
+
